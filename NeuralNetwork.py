770605be9ecbb554497e04eb934f6e7b2918727f
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-06-11 23:28:27 +0200

    refactor: add numpy and init weight matrices

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,6 +1,10 @@
 # neural network class definition
-
+import numpy;
 class NeuralNetwork:
+
+    # hnodes = hidden nodes
+    # inodes = input nodes
+    # onodes = output_nodes
 
     def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
         # number of nodes for each layer input, hidden and output
@@ -10,6 +14,40 @@ class NeuralNetwork:
 
         #learning rate
         self.lr = learningRate
+
+        #create weights
+        self.weights()
+        pass
+
+    # generate weight marices
+    def weights(self):
+
+        # O = Output Nodes
+        # I = Input Nodes
+        # H = Hidden Nodes
+
+        # link weight matrices:
+        # -0.5 in each statement takes care of creating  ranges[-0.5, 0.5]
+
+        # weights for input nodes and hidden nodes
+        # matrix is hnodes x inodes since weight matrix is multiplicated with input "vectors", resulting in values
+        # for the hidden layer:
+        #                    a                      au
+        #   I: u     w_i_h:  b    H: (w_i_h * I =)  bu
+        #                    c                      cu
+        #
+        #  w_i_h = 3 x 1
+        self.wih = numpy.random.rand(self.hnodes, self.inodes) - 0.5
+
+        # weights for hidden nodes and output nodes
+        # matrix is onodes x hnodes since H is multiplicated with weight matrix, resulting in values for O
+        #
+        #      au
+        #  H:  bu     w_h_o:  x y z      O: (H * w_h_o =)  x * au + y * bu + z * cu
+        #      cu
+        #
+        self.who = numpy.random.rand(self.onodes, self.hnodes) - 0.5
+
         pass
 
     # train the neural network
